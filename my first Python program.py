da29c76b2900a46c766dfc5b5ab5c6c5ad624637
Vader-75 <2022cwc@gmail.com>
2024-09-28 20:10:47 -0500

    Add files via upload

---
+++ my first Python program.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("I just wrote my first Python Program")
+print("This is so much fun")
+name= input("Hello, what is your name?") #gets the user's name
+print("Welcome, it is so nice to meet you Christopher")#prints a message with the user's name
+c = ("cats")
+d = ("dogs")
+s = print("It's raining cats and dogs!")
+
